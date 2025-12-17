61438c6d4af1b11eb9d28550c5ab3e89022c16e1
nakayamaken <bachboy0@gmail.com>
2025-12-17 13:39:37 +0900

    サービスワーカーの登録処理をmain.jsに移動し、index.htmlから削除

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// Service Worker 登録
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker 登録成功。スコープ:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.log("Service Worker 登録失敗:", error);
+      });
+  });
+}
+
 // 新幹線トグルロジック & Iframeリサイザー & 画像フォールバック
 document.addEventListener("DOMContentLoaded", () => {
   // Iframeのスケーリングロジック
